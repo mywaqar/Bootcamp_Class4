3e6ef5d3a1681afe8cc74bf5ec083ef5752dcfc2
Muhammad Ashraf <ashrafhussain786@yahoo.com>
2020-06-11 14:39:28 +0500

    save class4 file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
+
 
 function App() {
+  let [number, setNumber] = useState(0);
+  let [isLit, setLit] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`box ${isLit? 'light' : 'dark'}`}>
+      <Counter count={number} />
+      <br />
+      <button onClick={() => setNumber(++number)}>Increase Count</button>
+      <br />
+      <br />
+      <button onClick={() => setNumber(--number)}>Decrease Count</button>
+      <br />
+      <br />
+      <button onClick={() => setLit(!isLit)}>Set Light</button>
+      <h1>Good {isLit? 'Morning' : 'Night'}</h1>      
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Counter(props){
+    return(
+        <h1>Number in counter is : {props.count}</h1>
+    )
+}
+
+export default Counter;
